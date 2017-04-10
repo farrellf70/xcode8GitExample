56099f77766a5194aa28a482e35c3c2c53358e76
Frank Farrell <frankfarrell@Franks-MacBook-Pro.local>
2017-04-10 10:54:18 +0100

    Initial Commit

---
+++ xcode8GitExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xcode8GitExample
+//
+//  Created by Frank Farrell on 10/04/2017.
+//  Copyright © 2017 Frank Farrell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xcode8GitExample
+//
+//  Created by Frank Farrell on 10/04/2017.
+//  Copyright © 2017 Frank Farrell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
